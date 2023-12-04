1503e6eec93788822bd73090fb6527653d3037f0
YOUNS 777 <ben.YNS20@gmail.com>
2023-12-04 13:41:40 +0100

    04.12 13:41

--- automate.c
+++ automate.c
@@ -9,7 +9,7 @@ typedef struct {
 
 // Function to initialize an automaton (to be called before use)
 void initializeAutomaton(Automaton* automaton, int numberOfStates, int numberOfEvents) {
-    int i, j;
+    int i, j, k;
 
     automaton->numberOfStates = numberOfStates;
     automaton->numberOfEvents = numberOfEvents;
@@ -19,12 +19,19 @@ void initializeAutomaton(Automaton* automaton, int numberOfStates, int numberOfE
     for (i = 0; i < numberOfStates; i++) {
         automaton->transitionMatrix[i] = (int**)malloc(numberOfEvents * sizeof(int*));
         for (j = 0; j < numberOfEvents; j++) {
-            automaton->transitionMatrix[i][j] = (int*)malloc(numberOfStates * sizeof(int));
+            automaton->transitionMatrix[i][j] = (int*)calloc(numberOfStates, sizeof(int)); // Using calloc for zero-initialization
+            if (!automaton->transitionMatrix[i][j]) {
+                // Handle memory allocation error...
+            }
         }
     }
 
     // Initialization of the final states
-    automaton->finalStates = (int*)malloc(numberOfStates * sizeof(int));
+    automaton->finalStates = (int*)calloc(numberOfStates, sizeof(int)); // Using calloc for zero-initialization
+    if (!automaton->finalStates) {
+        // Handle memory allocation error...
+    }
+    automaton->initialState = -1; // Initialize to an invalid state
 }
 
 // Function to free the memory allocated to an automaton
@@ -42,6 +49,93 @@ void freeAutomaton(Automaton* automaton) {
 
     // Freeing the memory of the final states
     free(automaton->finalStates);
+    // Resetting the structure to avoid dangling pointers
+    automaton->transitionMatrix = NULL;
+    automaton->finalStates = NULL;
+    automaton->numberOfStates = 0;
+    automaton->numberOfEvents = 0;
+    automaton->initialState = -1;
+}
+
+// Fonction auxiliaire pour modifier une transition
+void modifierTransition(Automaton* automaton) {
+    int etatDepart, event, etatArrivee;
+
+    printf("Entrer l'état de départ: ");
+    scanf("%d", &etatDepart);
+    printf("Entrer l'indice de l'événement: ");
+    scanf("%d", &event);
+    printf("Entrer l'état d'arrivée: ");
+    scanf("%d", &etatArrivee);
+
+    if (etatDepart >= 0 && etatDepart < automaton->numberOfStates &&
+        event >= 0 && event < automaton->numberOfEvents &&
+        etatArrivee >= 0 && etatArrivee < automaton->numberOfStates) {
+        automaton->transitionMatrix[etatDepart][event][etatArrivee] = 1;
+    } else {
+        printf("Indices invalides!\n");
+    }
+}
+
+// Fonction auxiliaire pour modifier les états finaux
+void modifierEtatsFinaux(Automaton* automaton) {
+    int etat, estFinal;
+
+    printf("Entrer l'indice de l'état à modifier: ");
+    scanf("%d", &etat);
+    printf("Cet état est-il final? (1 pour oui, 0 pour non): ");
+    scanf("%d", &estFinal);
+
+    if (etat >= 0 && etat < automaton->numberOfStates) {
+        automaton->finalStates[etat] = estFinal;
+    } else {
+        printf("Indice d'état invalide!\n");
+    }
+}
+
+// Fonction auxiliaire pour modifier l'état initial
+void modifierEtatInitial(Automaton* automaton) {
+    int etatInitial;
+
+    printf("Entrer le nouvel état initial: ");
+    scanf("%d", &etatInitial);
+
+    if (etatInitial >= 0 && etatInitial < automaton->numberOfStates) {
+        automaton->initialState = etatInitial;
+    } else {
+        printf("Indice d'état invalide!\n");
+    }
+}
+
+// Fonction principale pour modifier l'automate
+void modifierAutomate(Automaton* automaton) {
+    int choix;
+
+    printf("Modifier l'automate:\n");
+    printf("1. Modifier une transition\n");
+    printf("2. Modifier les états finaux\n");
+    printf("3. Modifier l'état initial\n");
+    printf("4. Quitter\n");
+    printf("Choisir une option: ");
+    scanf("%d", &choix);
+
+    switch (choix) {
+        case 1:
+            modifierTransition(automaton);
+            break;
+        case 2:
+            modifierEtatsFinaux(automaton);
+            break;
+        case 3:
+            modifierEtatInitial(automaton);
+            break;
+        case 4:
+            printf("Sortie du mode de modification.\n");
+            break;
+        default:
+            printf("Option invalide!\n");
+            break;
+    }
 }
 
 // Function to serialize the automaton into a file
@@ -72,6 +166,34 @@ void deserializeAutomaton(const char* fileName, Automaton* automaton) {
     fclose(file);
 }
 
+// Fonction pour afficher l'automate
+void afficherAutomate(const Automaton* automaton) {
+    printf("Nombre d'états : %d\n", automaton->numberOfStates);
+    printf("Nombre d'événements : %d\n", automaton->numberOfEvents);
+    printf("État initial : %d\n", automaton->initialState);
+    printf("Matrice de transition :\n");
+
+    for (int i = 0; i < automaton->numberOfStates; i++) {
+        for (int j = 0; j < automaton->numberOfEvents; j++) {
+            printf("Depuis l'état %d avec l'événement %d : ", i, j);
+            for (int k = 0; k < automaton->numberOfStates; k++) {
+                if (automaton->transitionMatrix[i][j][k] != 0) {
+                    printf("%d ", k);
+                }
+            }
+            printf("\n");
+        }
+    }
+
+    printf("États finaux : ");
+    for (int i = 0; i < automaton->numberOfStates; i++) {
+        if (automaton->finalStates[i] != 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
 int main() {
     // Initializing the Automaton structure
     Automaton myAutomaton;
@@ -88,6 +210,8 @@ int main() {
     myAutomaton.finalStates[0] = 1;  // State 0 is final
     myAutomaton.finalStates[2] = 1;  // State 2 is final
     myAutomaton.initialState = 0;
+
+    afficherAutomate(&myAutomaton);
 
     // Serializing the automaton into a file
     serializeAutomaton("automaton.dat", &myAutomaton);
